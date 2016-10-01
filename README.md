fe2d090aa16511923f17f581ae3baf10de6a3013
RB Embleton <RBEmbleton@aol.com>
2016-09-30 20:35:26 -0400

    Update playing field css

--- README.md
+++ README.md
@@ -20,3 +20,7 @@
 ## Description ##
 
 Full Stack Blackjack is a full stack version of Blackjack. It allows users to create an account and then create and join games, playing against a dealer and other players. Using Pusher, Full Stack Blackjack allows users to see changes in real time allowing them to have a more realistic gaming experience.
+
+## Screenshots ##
+
+![playing_field](docs/playing_field.png)
--- javascript/components/playing_field.jsx
+++ javascript/components/playing_field.jsx
@@ -3,10 +3,32 @@ import VisibleUserDisplay from '../containers/visible_user_display';
 import VisibleDealerDisplay from '../containers/visible_dealer_display';
 
 const PlayingField = ({ game, users, dealer }) => {
+
+  function playerMap () {
+    const userMap = Object.keys(users).map((userId, idx) => {
+      return <VisibleUserDisplay key={idx} myClass="user-display" userId={parseInt(userId)}/>;
+    });
+    const numUsers = Object.keys(users).length
+
+    return (
+      <div className="users-display">
+        <div className="first-row">
+          {numUsers > 1 ? userMap.slice(0, 1).concat(userMap.slice(-1)) : userMap[0]}
+        </div>
+        {numUsers > 2 ?
+          <div className="second-row">
+            {userMap.slice(1, -1)}
+          </div> : null
+        }
+      </div>
+    );
+  }
+  // {Object.keys(users).map((userId, idx) => <VisibleUserDisplay key={idx} myClass="user-display" userId={parseInt(userId)}/>)}
+
   return (
     <div className="playing-field">
       <VisibleDealerDisplay myClass="dealer-display" dealerId={dealer.id} />
-      {Object.keys(users).map((userId, idx) => <VisibleUserDisplay key={idx} myClass="user-display" userId={parseInt(userId)}/>)}
+      {playerMap()}
     </div>
   );
 
